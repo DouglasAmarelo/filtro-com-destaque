8a4517b364bff42cff80fa0c1ba67ad7e0678c9d
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2020-04-04 21:47:36 -0300

    Destaque na pesquisa

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 (function () {
   'use strict';
 
-  console.log('Script is runing...');
+  const $search = document.querySelector('.search');
+  const $list = document.querySelector('.cidades');
+  const $listItens = $list.querySelectorAll('li');
+
+  $search.addEventListener('input', (e) => {
+    const searchTerm = e.target.value.trim();
+    const searchRegex = new RegExp(`(${searchTerm})`, 'gmi');
+
+    $listItens.forEach((item) => {
+      if (!searchTerm) return (item.innerHTML = item.textContent);
+
+      item.innerHTML = item.textContent.replace(
+        searchRegex,
+        '<span class="selected">$1</span>'
+      );
+    });
+  });
 })();
